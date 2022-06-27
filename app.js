b8bed0de2a97f8a7d5543fd76c1b58415d723df7
Anshul Chittora <60209547+starac9@users.noreply.github.com>
2022-06-28 01:34:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function fun() {
+  window.location.href = "page2.html";
+}
+
+function fun1() {
+  window.location.href = "page3.html";
+}
+
+function fun2() {
+  window.location.href = "page4.html";
+}
+
+function fun3() {
+  window.location.href = "page5.html";
+}
